0d52657bd9bd4b247726038f54d14ebdf56a8e02
Kentaro Kaneki <kentarokaneki@gmail.com>
2020-05-01 17:09:57 -0400

    Initial commit

---
+++ src/main/java/org/jeremy_kentaro/http_server/Main.java
@@ -0,0 +1,7 @@
+package org.jeremy_kentaro.http_server;
+
+public class Main {
+  public static void main(String[] args){
+    System.out.println("Started!!!");
+  }
+}
---
+++ src/test/java/org/jeremy_kentaro/http_server/MainTest.java
@@ -0,0 +1,12 @@
+package org.jeremy_kentaro.http_server;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+  @Test
+  public void something(){
+    assertTrue(true);
+  }
+}
